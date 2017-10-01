4b6977e78f3a1c357fc88685f883cea8d8a22e16
nbart1 <nicholas.barty@my.jcu.edu.au>
2017-10-01 10:57:36 +1000

    Add methods init, str, add_song & load_songs to songlist.py | Add test for adding a song in test_songlist.py

--- songlist.py
+++ songlist.py
@@ -1,5 +1,40 @@
 # create your SongList class in this file
+import csv
+from song import Song
 
 
 class SongList:
-    pass
+    def __init__(self):
+        self.songs = []
+
+    def __str__(self):
+        song_to_print = ""
+        for song in self.songs:
+            song_to_print += str(song)
+            song_to_print += "\n"
+        return song_to_print
+
+    def get_song_by_title(self):
+        pass
+
+    def add_song(self, song):
+        self.songs += [song]
+
+    def get_number_of_required_songs(self):
+        pass
+
+    def get_number_of_learned_songs(self):
+        pass
+
+    def load_songs(self, file_name):
+        with open(file_name, "r") as open_file:
+            read_file = csv.reader(open_file, delimiter=',')
+            for row in read_file:
+                song_to_load = Song(row[0], row[1], int(row[2]), True if row[3] == "y" else False)
+                self.add_song(song_to_load)
+
+    def save_songs(self):
+        pass
+
+    def sort(self):
+        pass
--- test_songlist.py
+++ test_songlist.py
@@ -4,6 +4,8 @@
 from songlist import SongList
 from song import Song
 
+song = Song("Amazing Grace", "John Newton", 1779, True)
+
 # test empty SongList
 song_list = SongList()
 print(song_list)
@@ -11,13 +13,17 @@ assert len(song_list.songs) == 0
 
 # test loading songs
 song_list.load_songs('songs.csv')
-print(song_list)
+print("Loaded SongList: \n{}".format(song_list))
 assert len(song_list.songs) > 0  # assuming CSV file is not empty
 
 # TODO: add tests below to show the various required methods work as expected
 # test sorting songs
 
 # test adding a new Song
+print("----Testing add_song function----")
+print("Adding {} to SongList".format(song))
+song_list.add_song(song)
+print("Updated SongList is: \n\n{}".format(song_list))
 
 # test get_song()
 
