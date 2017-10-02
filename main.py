6ee0571f6f1cd6ef1e1aa923eb689292943a328d
nbart1 <nicholas.barty@my.jcu.edu.au>
2017-10-02 16:43:29 +1000

    Add a general GUI layout to work from

--- main.py
+++ main.py
@@ -10,11 +10,15 @@ GitHub URL: https://github.com/CP1404-2017-2/a2-nbart1
 """
 
 from kivy.app import App
+from kivy.lang import Builder
 
 # Create your main program in this file, using the SongsToLearnApp class
 
 
 class SongsToLearnApp(App):
-    pass
+    def build(self):
+        self.title = "Songs To Learn 2.0"
+        self.root = Builder.load_file('app.kv')
+        return self.root
 
 SongsToLearnApp().run()
