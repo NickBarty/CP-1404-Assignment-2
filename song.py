d319b5aa85eaf98d6f901a993c40160607055d34
nbart1 <nicholas.barty@my.jcu.edu.au>
2017-10-08 22:47:23 +1000

    Cleanup docstrings

--- song.py
+++ song.py
@@ -1,25 +1,43 @@
 class Song:
-    """Song class initialises the attributes every song will have, how they will be printed and the ability to mark a
-    song as required or learned"""
+    """
+    Song class initialises the attributes every song will have, how they will be printed and the ability to mark a
+    song as required or learned
+    """
 
     def __init__(self, title="", artist="", year=0, required=True):
-        """initialises the attributes songs will have with default values"""
+        """
+        initialises the attributes songs will have with default values
+        :param title: title of the song
+        :param artist: artist of the song
+        :param year: year the song was made
+        :param required: if the song is learned or unlearned (defaults True for un-learned)
+        :return: None
+        """
         self.title = title
         self.artist = artist
         self.year = year
         self.required = required
 
     def __str__(self):
-        """specifies how songs will be printed and if they are learned or not"""
+        """
+        specifies how songs will be printed and if they are learned or not
+        :return: formatted string
+        """
         check_learned = "" if self.required else "(learned)"
         return '"{}" by {} ({}) {}'.format(self.title, self.artist, self.year, check_learned)
 
     def mark_required(self):
-        """marks the song as required (not learned)"""
+        """
+        marks the song as required (not learned)
+        :return: song with required set to True (not learned)
+        """
         self.required = True
         return self.required
 
     def mark_learned(self):
-        """marks the song as learned"""
+        """
+        marks the song as learned
+        :return: song with required set to False (learned)
+        """
         self.required = False
         return self.required
--- songlist.py
+++ songlist.py
@@ -4,40 +4,67 @@ from operator import attrgetter
 
 
 class SongList:
-    """SongList class initialises a list for songs to be stored in, specifies how to print the song list and gives the
+    """
+    SongList class initialises a list for songs to be stored in, specifies how to print the song list and gives the
     ability to find a song by its title, add a song to the list, get the number of required/learned songs in the list,
-    load & save the CSV file and sort the song list by specific attributes"""
+    load & save the CSV file and sort the song list by specific attributes
+    """
 
     def __init__(self):
-        """initialises a list for songs to be stored in"""
+        """
+        initialises a list for songs to be stored in
+        :return: None
+        """
         self.songs = []
 
     def __str__(self):
-        """specifies how to print the song list"""
+        """
+        specifies how to print the song list
+        :return: formatted songs list string
+        """
         songs_to_print = ""
         for song in self.songs:
             songs_to_print += "{} \n".format(song)
         return songs_to_print
 
     def get_song_by_title(self, song_to_try):
-        """get all attributes of a song by entering in just the title"""
+        """
+        get all attributes of a song by entering in just the title
+        :param song_to_try: song to find title in
+        :return: song
+        """
         for song in self.songs:
             if song.title in song_to_try:
                 return song
 
     def add_song(self, song):
+        """
+        add a song to the song list
+        :param song: song to add to song list
+        :return: None
+        """
         self.songs += [song]
 
     def get_number_of_required_songs(self):
-        """gets number of songs that are required (haven't been learnt) and returns that number"""
+        """
+        gets number of songs that are required (haven't been learnt) and returns that number
+        :return: number of required (not learnt) songs
+        """
         return len([song for song in self.songs if song.required])
 
     def get_number_of_learned_songs(self):
-        """gets number of songs that have been learnt and returns that number"""
+        """
+        gets number of songs that have been learnt and returns that number
+        :return: number of learned songs
+        """
         return len([song for song in self.songs if not song.required])
 
     def load_songs(self, file_name):
-        """reads CSV file line by line, adding each line (song) to the song list"""
+        """
+        reads a CSV file line by line, adding each line (song) to the song list
+        :param file_name: name of file to load
+        :return: None
+        """
         with open(file_name, "r") as open_file:
             read_file = csv.reader(open_file, delimiter=',')
             for row in read_file:
@@ -45,12 +72,20 @@ class SongList:
                 self.songs.append(song_to_load)
 
     def save_songs(self, file_name):
-        """writes CSV file line by line using the attributes from each song in the list"""
+        """
+        writes CSV file line by line using the attributes from each song in the list
+        :param file_name: name of file to save to
+        :return: None
+        """
         with open(file_name, "w") as open_file:
             write_to_file = csv.writer(open_file, delimiter=",", lineterminator="\n")
             for song in self.songs:
                 write_to_file.writerow([song.title, song.artist, song.year, "y" if song.required else "n"])
 
     def sort(self, attribute):
-        """sorts the song list by the specified attribute (title, artist, year, is_required) and then by the title"""
+        """
+        sorts the song list by the specified attribute (title, artist, year, is_required) and then by the title
+        :param attribute: attribute to sort the songs by
+        :return: None
+        """
         self.songs.sort(key=attrgetter(attribute.lower(), "title"))
